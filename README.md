d8a12c273a176ceecfab59522985b46901bf7bf2
Shingo Omura <everpeace@gmail.com>
2014-04-26 04:49:49 -0700

    Redefine Faceted by using Free monad.

--- README.md
+++ README.md
@@ -28,44 +28,71 @@ please see [test](https://github.com/everpeace/faceted-values/blob/master/test/D
 
 or, test log could be useful to understand how-to easily.
 ```
+Test suite spec: RUNNING...
+
 Data.Faceted
-  faceted value simple =  Faceted (\x -> x > 0) (1, 0) (which is equivalent with < (x > 0) ? 1 : 0>)
-    - observation with context 1 should be 1
-    - observation with context 0 should be 0
+      facete value can be declared in intuitive manner "(\x -> x > 0) ? 1 .: 0)"
+        - its observation with context 0 should be 0.
+        - its observation with context 1 should be 1.
 
-  Functor: ((*3) `fmap` simple) should be equivalent with < (x > 0) ? 1*3 : 0*3>
-    - observation with context 1 should be 3
-    - observation with context 0 should be 0
+      use (??) for nested facete value "(\x -> x <= 2) ?? ((\x -> x < 2) ? 1 .: 2) .: ((\x -> x < 4 ) ? 3 .: 4)"
+        - its observation with context 1 should be 1.
+        - its observation with context 2 should be 2.
+        - its observation with context 3 should be 3.
+        - its observation with context 4 should be 4.
 
-  Applicative: ((+) <$> simple <*> simple) should  be equivalent with < (x > 0) ? 1+1 : 0+0>
-    - observation with context 1 should be 3
-    - observation with context 0 should be 0
+      Functor: ((*3) `fmap` (\x -> x > 0) ? 1 .: 0)) should be equivalent with < (x > 0) ? 1*3 : 0*3>.
+        - observation with context 0 should be 0.
+        - observation with context 1 should be 3.
 
-  Applicative Do:
-         do a <- simple
-            b <- simple
-            return a+b
-        should be equivalent with
-          < (x > 0) ? 1+1 : 0+0>
-    - observation with context 1 should be 3
-    - observation with context 0 should be 0
+      Applicative: ((+) <$> ((\x -> 0 < x && x < 3) ? 1 .: 2) <*> ((\x -> 1 < x && x < 4) ? 4 .: 8))
+      This computation adds two faceted values. So in this case, 4 patterns of results can be observed.
+      The result should be equivalent with
+        < (0 < x < 3) ? < (1 < x < 4) ? 1+4 : 1+8 >
+                      : < (1 < x < 4) ? 2+4 : 2+8 >>
+        - observation with context 1 should be 9  (= 1 + 8).
+        - observation with context 2 should be 5  (= 1 + 4).
+        - observation with context 3 should be 6  (= 2 + 4).
+        - observation with context 4 should be 10 (= 2 + 8).
 
-  Bind: simple >>= (\v -> Faceted (\y -> y < 2) (v + 1, v + 3))
-        shoule be equivalent with
-          < (x > 0) ? < (y < 2) ? 1+1, 1+3> : < (y < 2) ? 0+1, 0+3> >
-    - observation with context 2 should be 4
-    - observation with context 1 should be 2
-    - observation with context 0 should be 0
+      Applicative Do:
+       do a <- ((\x -> 0 < x && x < 3) ? 1 .: 2)
+          b <- ((\x -> 1 < x && x < 4) ? 4 .: 8)
+          return a + b
+      should be equivalent with above.
 
-  Do Syntax:
-        do a <- simple
-           b <- simple
-           Faceted (\y -> y < 2) (a*b, a+b)
-        shoule be equivalent with
-          < (x > 0) ? < (y < 2) ? 1*1, 1+1> : < (y < 2) ? 0*0, 0-0> >
-    - observation with context 2 should be 2
-    - observation with context 1 should be 1
-    - observation with context 0 should be 0
+        - observation with context 1 should be 9  (= 1 + 8).
+        - observation with context 2 should be 5  (= 1 + 4).
+        - observation with context 3 should be 6  (= 2 + 4).
+        - observation with context 4 should be 10 (= 2 + 8).
+
+      Bind: ((\x -> 0 < x && x < 3) ? 1 .: 2) >>= (\v -> ((\x -> 1 < x && x < 4) ? (v+4) .: (v+8))
+      shoule be equivalent with
+        < (0 < x < 3) ? < (1 < x < 4) ? 1+4 : 1+8 >
+                      : < (1 < x < 4) ? 2+4 : 2+8 >>
+        - observation with context 1 should be 9  (= 1 + 8).
+        - observation with context 2 should be 5  (= 1 + 4).
+        - observation with context 3 should be 6  (= 2 + 4).
+        - observation with context 4 should be 10 (= 2 + 8).
+
+      Do Syntax:
+      do a <- ((\x -> 0 < x && x < 3) ? 1 .: 2)
+         b <- ((\x -> 1 < x && x < 4) ? (a+4) .: (a+8))
+         (\y -> y < 3) ? (10*b) .: (100*b)
+      shoule be equivalent with
+        < (0 < x < 3) ? < (1 < x < 4) ? <(y < 3)? 10*(1+4) : 100*(1+4)>
+                                      : <(y < 3)? 10*(2+8) : 100*(2+8)>>
+                      : < (1 < x < 4) ? <(y < 3)? 10*(2+4) : 100*(2+4)>
+                                      : <(y < 3)? 10*(2+8) : 100*(2+8)>>
+        - observation with context 1 should be 9  (= 1 + 8).
+        - observation with context 2 should be 5  (= 1 + 4).
+        - observation with context 3 should be 6  (= 2 + 4).
+        - observation with context 4 should be 10 (= 2 + 8).
+
+Finished in 0.0011 seconds
+24 examples, 0 failures
+Test suite spec: PASS
+Test suite logged to: dist/test/faceted-0.1.0.0-spec.log
 
 Finished in 0.0011 seconds
 14 examples, 0 failures
--- test/Data/FacetedSpec.hs
+++ test/Data/FacetedSpec.hs
@@ -4,76 +4,99 @@ import Control.Applicative
 import Data.Faceted
 import Test.Hspec
 
--- Simple Faceted Value < (\x -> x >= 1) ? 1 : 0>
-simple:: Faceted Int Int
-simple = Faceted (\x -> x > 0) 1 0
-
+-- Simple Faceted Value
+simple = (\x -> x > 0) ? 1 .: 0
+nested = (\x -> x <= 2) ?? ((\x -> x < 2) ? 1 .: 2) .: ((\x -> x < 4 ) ? 3 .: 4)
 -- do syntax test cases
-ap_do :: Faceted Int Int
-ap_do = do a <- simple
-           b <- simple
-           return (a+b)
-monad_do :: Faceted Int Int
-monad_do = do a <- simple
-              b <- simple
-              Faceted (\y -> y < 2) (a*b) (a+b)
+ap_do = do a <- ((\x -> 0 < x && x < 3) ? 1 .: 2)
+           b <- ((\x -> 1 < x && x < 4) ? 4 .: 8)
+           return (a + b)
+monad_do = do a <- ((\x -> 0 < x && x < 3) ? 1 .: 2)
+              b <- ((\x -> 1 < x && x < 4) ? (a+4) .: (a+8))
+              (\y -> y < 3) ? (10*b) .: (100*b)
 
 spec :: Spec
 spec = do
-  describe "faceted value simple =  Faceted (\\x -> x > 0) 1 0 (which is equivalent with < (x > 0) ? 1 : 0>)" $ do
-    it "observation with context 1 should be 1" $
+  describe "facete value can be declared in intuitive manner \"(\\x -> x > 0) ? 1 .: 0)\"" $ do
+    it "its observation with context 0 should be 0." $
+      observe simple 0 `shouldBe` 0
+    it "its observation with context 1 should be 1." $
       observe simple 1 `shouldBe` 1
 
-    it "observation with context 0 should be 0" $
-      observe simple 0 `shouldBe` 0
 
+  describe "use (??) for nested facete value \"(\\x -> x <= 2) ?? ((\\x -> x < 2) ? 1 .: 2) .: ((\\x -> x < 4 ) ? 3 .: 4)\"" $ do
+    it "its observation with context 1 should be 1." $
+      observe nested 1 `shouldBe` 1
+    it "its observation with context 2 should be 2." $
+      observe nested 2 `shouldBe` 2
+    it "its observation with context 3 should be 3." $
+      observe nested 3 `shouldBe` 3
+    it "its observation with context 4 should be 4." $
+      observe nested 4 `shouldBe` 4
 
-  describe "Functor: ((*3) `fmap` simple) should be equivalent with < (x > 0) ? 1*3 : 0*3> " $ do
-    it "observation with context 1 should be 3" $
+  describe "Functor: ((*3) `fmap` (\\x -> x > 0) ? 1 .: 0)) should be equivalent with < (x > 0) ? 1*3 : 0*3>." $ do
+    it "observation with context 0 should be 0." $
+      observe ((*3) `fmap` simple) 0 `shouldBe` 0
+    it "observation with context 1 should be 3." $
       observe ((*3) `fmap` simple) 1 `shouldBe` 3
 
-    it "observation with context 0 should be 0" $
-      observe ((*3) `fmap` simple) 0 `shouldBe` 0
 
-
-  describe "Applicative: ((+) <$> simple <*> simple) should  be equivalent with < (x > 0) ? 1+1 : 0+0>" $ do
-    it "observation with context 1 should be 3" $
-      observe ((+) <$> simple <*> simple) 1 `shouldBe` 2
-
-    it "observation with context 0 should be 0" $
-      observe ((+) <$> simple <*> simple) 0 `shouldBe` 0
+  describe ("Applicative: ((+) <$> ((\\x -> 0 < x && x < 3) ? 1 .: 2) <*> ((\\x -> 1 < x && x < 4) ? 4 .: 8))\n"
+          ++"\tThis computation adds two faceted values. So in this case, 4 patterns of results can be observed.\n"
+          ++"\tThe result should be equivalent with\n"
+          ++"\t  < (0 < x < 3) ? < (1 < x < 4) ? 1+4 : 1+8 >\n"
+          ++"\t                : < (1 < x < 4) ? 2+4 : 2+8 >>") $ do
+    it "observation with context 1 should be 9  (= 1 + 8)." $
+      observe ( (+) <$> ((\x -> 0 < x && x < 3) ? 1 .: 2) <*> ((\x -> 1 < x && x < 4) ? 4 .: 8)) 1 `shouldBe` 9
+    it "observation with context 2 should be 5  (= 1 + 4)." $
+      observe ( (+) <$> ((\x -> 0 < x && x < 3) ? 1 .: 2) <*> ((\x -> 1 < x && x < 4) ? 4 .: 8)) 2 `shouldBe` 5
+    it "observation with context 3 should be 6  (= 2 + 4)." $
+      observe ( (+) <$> ((\x -> 0 < x && x < 3) ? 1 .: 2) <*> ((\x -> 1 < x && x < 4) ? 4 .: 8)) 3 `shouldBe` 6
+    it "observation with context 4 should be 10 (= 2 + 8)." $
+      observe ( (+) <$> ((\x -> 0 < x && x < 3) ? 1 .: 2) <*> ((\x -> 1 < x && x < 4) ? 4 .: 8)) 4 `shouldBe` 10
 
   describe ("Applicative Do: \n"
-          ++"\t do a <- simple\n"
-          ++"\t    b <- simple\n"
-          ++"\t    return a+b\n"
-          ++"\tshould be equivalent with\n"
-          ++"\t  < (x > 0) ? 1+1 : 0+0>") $ do
-    it "observation with context 1 should be 3" $
-      observe ap_do 1 `shouldBe` 2
-    it "observation with context 0 should be 0" $
-      observe ap_do 0 `shouldBe` 0
+          ++"\t do a <- ((\\x -> 0 < x && x < 3) ? 1 .: 2)\n"
+          ++"\t    b <- ((\\x -> 1 < x && x < 4) ? 4 .: 8)\n"
+          ++"\t    return a + b\n"
+          ++"\tshould be equivalent with above.\n") $ do
+    it "observation with context 1 should be 9  (= 1 + 8)." $
+      observe ap_do 1 `shouldBe` 9
+    it "observation with context 2 should be 5  (= 1 + 4)." $
+      observe ap_do 2 `shouldBe` 5
+    it "observation with context 3 should be 6  (= 2 + 4)." $
+      observe ap_do 3 `shouldBe` 6
+    it "observation with context 4 should be 10 (= 2 + 8)." $
+      observe ap_do 4 `shouldBe` 10
 
-  describe ("Bind: simple >>= (\\v -> Faceted (\\y -> y < 2) (v + 1) (v + 3))\n"
+
+  describe ("Bind: ((\\x -> 0 < x && x < 3) ? 1 .: 2) >>= (\\v -> ((\\x -> 1 < x && x < 4) ? (v+4) .: (v+8))\n"
           ++"\tshoule be equivalent with\n"
-          ++"\t  < (x > 0) ? < (y < 2) ? 1+1, 1+3> : < (y < 2) ? 0+1, 0+3> >") $ do
-    it "observation with context 2 should be 4" $
-      observe (simple >>= (\v -> Faceted (\y -> y < 2) (v + 1) (v + 3))) 2 `shouldBe` 4
-    it "observation with context 1 should be 2" $
-      observe (simple >>= (\v -> Faceted (\y -> y < 2) (v + 1) (v + 3))) 1 `shouldBe` 2
-    it "observation with context 0 should be 0" $
-      observe (simple >>= (\v -> Faceted (\y -> y < 2) (v + 1) (v + 3))) 0 `shouldBe` 1
-
+          ++"\t  < (0 < x < 3) ? < (1 < x < 4) ? 1+4 : 1+8 >\n"
+          ++"\t                : < (1 < x < 4) ? 2+4 : 2+8 >>") $ do
+    it "observation with context 1 should be 9  (= 1 + 8)." $
+      observe (((\x -> 0 < x && x < 3) ? 1 .: 2) >>= \v -> ((\x -> 1 < x && x < 4) ? (v+4) .: (v+8))) 1 `shouldBe` 9
+    it "observation with context 2 should be 5  (= 1 + 4)." $
+      observe (((\x -> 0 < x && x < 3) ? 1 .: 2) >>= \v -> ((\x -> 1 < x && x < 4) ? (v+4) .: (v+8))) 2 `shouldBe` 5
+    it "observation with context 3 should be 6  (= 2 + 4)." $
+      observe (((\x -> 0 < x && x < 3) ? 1 .: 2) >>= \v -> ((\x -> 1 < x && x < 4) ? (v+4) .: (v+8))) 3 `shouldBe` 6
+    it "observation with context 4 should be 10 (= 2 + 8)." $
+      observe (((\x -> 0 < x && x < 3) ? 1 .: 2) >>= \v -> ((\x -> 1 < x && x < 4) ? (v+4) .: (v+8))) 4 `shouldBe` 10
 
   describe ("Do Syntax:\n"
-          ++"\tdo a <- simple\n"
-          ++"\t   b <- simple\n"
-          ++"\t   Faceted (\\y -> y < 2) (a*b) (a+b)\n"
+          ++"\tdo a <- ((\\x -> 0 < x && x < 3) ? 1 .: 2)\n"
+          ++"\t   b <- ((\\x -> 1 < x && x < 4) ? (a+4) .: (a+8))\n"
+          ++"\t   (\\y -> y < 3) ? (10*b) .: (100*b)\n"
           ++"\tshoule be equivalent with\n"
-          ++"\t  < (x > 0) ? < (y < 2) ? 1*1, 1+1> : < (y < 2) ? 0*0, 0-0> >") $ do
-    it "observation with context 2 should be 2" $
-      observe monad_do 2 `shouldBe` 2
-    it "observation with context 1 should be 1" $
-      observe monad_do 1 `shouldBe` 1
-    it "observation with context 0 should be 0" $
-      observe monad_do 0 `shouldBe` 0
+          ++"\t  < (0 < x < 3) ? < (1 < x < 4) ? <(y < 3)? 10*(1+4) : 100*(1+4)>\n"
+          ++"\t                                : <(y < 3)? 10*(2+8) : 100*(2+8)>>\n"
+          ++"\t                : < (1 < x < 4) ? <(y < 3)? 10*(2+4) : 100*(2+4)>\n"
+          ++"\t                                : <(y < 3)? 10*(2+8) : 100*(2+8)>>") $ do
+    it "observation with context 1 should be 9  (= 1 + 8)." $
+      observe monad_do 1 `shouldBe` 90
+    it "observation with context 2 should be 5  (= 1 + 4)." $
+      observe monad_do 2 `shouldBe` 50
+    it "observation with context 3 should be 6  (= 2 + 4)." $
+      observe monad_do 3 `shouldBe` 600
+    it "observation with context 4 should be 10 (= 2 + 8)." $
+      observe monad_do 4 `shouldBe` 1000
